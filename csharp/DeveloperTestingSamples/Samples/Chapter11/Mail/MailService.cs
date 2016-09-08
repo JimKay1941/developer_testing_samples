f586ac6b447d70e8666e99b282515ae75bceb6dd
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2016-09-08 17:32:00 +0200

    Added the mail server C# example.

---
+++ csharp/DeveloperTestingSamples/Samples/Chapter11/Mail/MailService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Chapter11.Mail
+{
+    public class MailService
+    {
+        private const String Footer = "Kindest regards,\nCompany Support";
+
+        private readonly MailAddress FromAddress
+            = new MailAddress("support@company.local", "Company Support");
+
+        private SmtpClient smtpClient;
+
+        public MailService(String smtpServer)
+        {
+            smtpClient = new SmtpClient(smtpServer, 25);
+        }
+
+        public void SendMail(MailAddress toAddress, String subject, String body)
+        {
+            MailMessage mail = new MailMessage(FromAddress, toAddress);
+            mail.Subject = subject;
+            mail.Body = body + "\n" + Footer;
+            smtpClient.Send(mail);
+        }
+    }
+}
---
+++ csharp/DeveloperTestingSamples/SamplesTests/Chapter11/Mail/MailServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Mail;
+using netDumbster.smtp;
+using Samples.Chapter11.Mail;
+
+namespace SampleTests.Chapter11.Mail
+{
+    [TestClass]
+    public class MailServiceTest
+    {
+        private SimpleSmtpServer smtpServer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            smtpServer = SimpleSmtpServer.Start(25);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            smtpServer.Stop();
+        }
+
+        [TestMethod]
+        public void CompanyInformationIsPresentInEmail()
+        {
+            MailService testedService = new MailService("localhost");
+            testedService.SendMail(new MailAddress("user@test.local"), 
+                "Dear customer", "We care!");
+            
+            Assert.AreEqual(1, smtpServer.ReceivedEmailCount);
+            
+            SmtpMessage sentMail = (SmtpMessage)smtpServer.ReceivedEmail[0];
+            Assert.AreEqual("support@company.local", 
+                sentMail.FromAddress.ToString());
+            StringAssert.Contains(sentMail.Data, "Company Support");
+        }
+    }
+}
