312007b294b892a77e6b9913b08bbdfd5ffbbe5a
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2016-10-02 22:49:23 +0200

    Added the C# versions of the test doubles samples (chapter 12).

---
+++ csharp/DeveloperTestingSamples/Samples/Chapter12/Stubbing/ConcreteCollaborator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Samples.Chapter12.Stubbing
+{
+
+    
+    // Concrete implementation of the supposed collaborator with a method
+    // that returns something unpredictable.
+    public class ConcreteCollaborator : ICollaborator
+    {
+        public int ComputeAndReturnValue()
+        {
+            return DateTime.Now.Hour * 60;
+        }
+    }
+}
